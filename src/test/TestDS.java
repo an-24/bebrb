c5bdfc4ed367b7933732c349760adbd8b087a33a
an-24 <andrej@MacBook-Air-Andrej.local>
2013-11-07 00:26:02 +0600

    v.0.7.6

--- src/test/TestDS.java
+++ src/test/TestDS.java
@@ -1,7 +1,15 @@
 package test;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URL;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.w3c.dom.Document;
+
 
 public class TestDS {
 
@@ -9,10 +17,18 @@ public class TestDS {
 		try {
 			System.out.println("start...");
 			String xsdPath = "/org/bebrb/resources/shema/ds.xsd";
-			InputStream xsd = System.class.getResourceAsStream(xsdPath);
+			URL xsd = System.class.getResource(xsdPath);
 			if(xsd==null)
 				throw new IOException("XSD shema not found "+xsdPath);
-		} catch (IOException e) {
+			Schema schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(xsd); 
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setNamespaceAware(true);
+			factory.setSchema(schema);
+			factory.setValidating(false);
+			Document doc = factory.newDocumentBuilder().parse("src/test/datasources.xml");
+			
+			System.out.println("[ok]");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
